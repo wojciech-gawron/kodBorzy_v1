ef63ac28933d0c157a9ffa24c012dc481aeef49e
adolf <sraniee@izjadajnie.pl>
2026-05-04 18:49:40 +0200

    fdf

--- main.cpp
+++ main.cpp
@@ -29,6 +29,11 @@ int main(int argc, char* argv[]) {
             printf("%d - %d = %d\n", number1, number2, result);
             return 0;
         }
+        if (!strcmp("mod", argv[2])) {
+            result = number1 % number2;
+            printf("%d mod %d = %d\n", number1, number2, result);
+            return 0;
+        }
     }
 
     usage(argv[0]);
